1e8b7724192dd092bcf11511b3ae2aed7934da54
John Hotovy <jghotovy92@gmail.com>
2025-04-30 14:22:51 -0600

    Publish to github packages

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     `java-gradle-plugin`
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
 }
 
 group = "io.genstrings"
-version = "1.0-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 gradlePlugin {
     plugins {
@@ -27,6 +28,21 @@ dependencies {
     implementation(libs.ktor)
     implementation(libs.openai)
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    val githubPackagesUrl = "https://maven.pkg.github.com/jg-hot/genstrings"
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri(githubPackagesUrl)
+            credentials {
+                username = properties["gpr.user"]?.toString()
+                password = properties["gpr.key"]?.toString()
+            }
+        }
+    }
 }
 
 tasks.test {
